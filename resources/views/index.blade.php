7ad4294d439c3577d358491d6c0671d33b88b079
Badru <badruakfm@gmail.com>
2020-07-09 16:26:16 +0700

    tambah template admin

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title','Home')
+
+@section('content')
+<h1 class="h3 mb-4 text-gray-800">Home Page</h1>
+<img src="{{ asset('/images/blog.png') }}" alt="" srcset="">
+@endsection
