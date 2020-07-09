69c75bf9d8460728b7f4bb20b0cde1c5ac68d688
rahma yudistira <yudistira.anaga@gmail.com>
2020-07-09 22:26:43 +0700

    mengeluarkan semuadata pertanyaan pada route index pertanyaan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,38 @@
 @section('title','Home')
 
 @section('content')
-<h1 class="h3 mb-4 text-gray-800">Home Page</h1>
-<img src="{{ asset('/images/blog.png') }}" alt="" srcset="">
+<div class="row">
+    <div class="col-md-6">
+        <h1 class="h3 mb-4 text-gray-800">All Pertanyaan</h1>
+        <a href="/pertanyaan/create" class="btn btn-primary">buat pertanyaan</a>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">judul</th>
+                        <th scope="col">isi</th>
+                        <th scope="col">action</th>
+                    </tr>
+                </thead>
+                <tbody>
+        @foreach ($data as $d)        
+                    <tr>
+                        <td>{{$d->judul}}</td>
+                        <td>{{Str::limit($d->isi, 20)}}</td>
+                        <td>
+                            <a href="/pertanyaan/{{$d->id}}" class="btn btn-info">detail</a>
+                            <a href="/pertanyaan/{{$d->id}}/edit" class="btn btn-warning">ubah</a>
+                            <form action="/pertanyaan/{{$d->id}}" method="post" style="display: inline">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">hapus</button>
+                            </form>
+                        </td>
+                    </tr>
+        @endforeach
+                </tbody>
+            </table>
+            {{$data}}
+    </div>
+</div>
+
 @endsection
